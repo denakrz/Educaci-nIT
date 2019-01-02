f8c87f22817e7aca039a03fb95cb82bfbf4ab326
Denise Krzyny <denisek@lagash.com>
2019-01-02 17:25:29 -0300

    Clase 1

--- Js/dom.js
+++
@@ -1,9 +0,0 @@
-// Modelos de objetos del documento (DOM)
-
-// variable global
-//var nodo_caja = document.getElementById("caja1");
-var nodo_caja = document.querySelector("#example1"); // selector CSS
-console.log(nodo_caja);
-
-// manipular el elemento capturado: 
-nodo_caja.style.border = "3px solid red";
